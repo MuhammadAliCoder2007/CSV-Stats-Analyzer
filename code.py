7682f45ff170a7538d4f0d73c65a1122a924677d
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-02-03 20:40:43 -0500

    Implement CSV stats analyzer with validation, counting, and basic statistics

--- code.py
+++ code.py
@@ -5,23 +5,35 @@
 # compute min / max / average
 # not crash
 
-
 f = open("data.csv")
 
 valid = 0
 invalid = 0
+total =0
+min = 100
+max = -100
+
+next(f)
 # read the file
 for line in f:
     try:
         int(line)
-        print(line) #prints the valid rows which are numbers
+        # print(line) #prints the valid rows which are numbers
+        total+=int(line)
         valid+=1 #keeps the count of valid rows
+        if int(line)<min:
+            min = int(line)
+        if int(line)>max:
+            max = int(line)
     except ValueError:
         invalid+=1
         pass
 
 print(valid ,"are valid values")
 print(invalid, "are invalid values")
-
+if (valid!=0):
+    print("Average is " ,total/valid)
+print("Maximum value is ", max)
+print("Minimum value is ", min)
 
     
